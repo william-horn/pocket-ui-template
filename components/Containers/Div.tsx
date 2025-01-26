9b74fa970cdd3562c79db3cfdf4d92292ab39850
William Horn <williamjosephhorn@gmail.com>
2025-01-26 14:12:45 -0500

    CHECKPOINT working type system for type checking native component prop types and custom prop types

--- components/Containers/Div.tsx
+++ components/Containers/Div.tsx
@@ -3,9 +3,12 @@ import evaluateProps from "@/utils/pocket-ui/evaluateProps";
 import { ComponentProps } from "react";
 
 // Optional: Add more props unique to only this element
-export interface CustomDivProps extends ContainerProps {}
+export interface CustomDivProps extends ContainerProps {
+  // className?: number;
+}
+
 export type NativeDivProps = ComponentProps<"div">;
-export type DivProps = NativeDivProps & CustomDivProps;
+export type DivProps = NativeDivProps | CustomDivProps;
 
 const Div = (props: DivProps) => {
   // const inside = "from inside component";
@@ -16,7 +19,7 @@ const Div = (props: DivProps) => {
     CustomDivProps
   >(props, {
     nativeOverrides: {
-      className: (value) => 0,
+      className: (value) => "",
     },
     customOverrides: {},
   });
--- utils/pocket-ui/evaluateProps.ts
+++ utils/pocket-ui/evaluateProps.ts
@@ -1,13 +1,17 @@
-type PropUpdater<NativeProps> = (value: any) => any;
+type PropUpdater<Return> = (value: any) => Return;
 
-interface PropEvaluationOptions<NativeProps, CustomProps> {
-  nativeOverrides: { [key in keyof NativeProps]: PropUpdater<NativeProps> };
-  customOverrides: { [key in keyof CustomProps]: PropUpdater<NativeProps> };
-}
+type PropEvaluationOptions<NativeProps, CustomProps> = {
+  nativeOverrides: {
+    [key in keyof NativeProps]: PropUpdater<NativeProps[key]>;
+  };
+  customOverrides: {
+    [key in keyof CustomProps]: PropUpdater<CustomProps[key]>;
+  };
+};
 
-interface PropScope {
+type PropScope = {
   [key: string]: any;
-}
+};
 
 const evaluateProps = <Props, NativeProps, CustomProps>(
   props: Props,
